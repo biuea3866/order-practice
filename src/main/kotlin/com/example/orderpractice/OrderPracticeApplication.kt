7b78fb489d137084a72df6a842d835a70c5db8c9
biuea <biuea3866@gmail.com>
2023-10-28 17:47:27 +0900

    init project

---
+++ src/main/kotlin/com/example/orderpractice/OrderPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.orderpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrderPracticeApplication>(*args)
+}
